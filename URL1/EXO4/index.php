7e57fcffbeb6abb33326731e5d7cfe01134209a1
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-14 12:09:08 +1100

    finalisation EXO4

---
+++ URL1/EXO4/index.php
@@ -0,0 +1,12 @@
+<?php
+$string = "String";
+echo "$string <br>";
+$int = 15;
+echo "$int <br>";
+$float = 57.3;
+echo "$float <br>";
+$boolF = false;
+$boolT = true;
+echo "$boolF <br>";
+echo "$boolT <br>";
+?>
