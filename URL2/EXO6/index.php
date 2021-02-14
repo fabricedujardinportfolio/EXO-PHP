81b502a337890ca36e0d193f8bdd45dbc6026df1
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-14 14:47:20 +1100

    finalisation EXO6 EXO7  URL2

---
+++ URL2/EXO6/index.php
@@ -0,0 +1,13 @@
+<?php
+$age = 28;
+  echo ($age >= 18) ? 'Tu es majeur<br>' : 'Tu n\'es pas majeur<br>';
+?>
+
+<?php
+$age = 17;
+if($age >= 18){
+   echo 'Tu es majeur<br>';
+}else{
+    echo 'Tu n\'es pas majeur<br>';
+}
+?>
---
+++ URL2/EXO7/index.php
@@ -0,0 +1,14 @@
+<?php
+$isOk = true;
+  echo ($isOk == false) ? 'c\'est pas bon !!!<br>' : 'c\'est ok !!!<br>';
+?>
+
+
+<?php
+$isOk = false;
+if($isOk == false){
+   echo 'c\'est pas bon !!!<br>';
+}else{
+    echo 'c\'est ok !!!<br>';
+}
+?>
