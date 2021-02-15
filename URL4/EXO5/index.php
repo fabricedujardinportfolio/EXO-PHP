06fee782d25e474a02ee57cd5971a9885455ceef
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-15 18:51:49 +1100

    finalisation EXO5 URL4

---
+++ URL4/EXO5/index.php
@@ -0,0 +1,8 @@
+<?php
+function mariage($chaine,$nombre)
+{
+    # code...
+    echo"Veut tu m'épouser ? $chaine . <br>Mais dit moi depuit combient de temps attend tu de me poser cette question ?<br> $nombre ans <br>";
+}
+mariage("oui",15);
+?>
