dc2491307772d50f428fec5dfeb6468d1f5657e3
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-15 19:14:19 +1100

    finalisation EXO8 URL4

---
+++ URL4/EXO8/index.php
@@ -0,0 +1,16 @@
+<?php
+$nombre1 = 2;
+$nombre2 = 5;
+$nombre3 = 6;
+
+function calcul($nombre1,$nombre2,$nombre3)
+{
+    # code...
+    echo"$nombre1"+"$nombre2"+"$nombre3";
+    echo"<br>";
+}
+calcul(12,12,12);
+calcul(78,12,44);
+calcul(12,15,12);
+calcul(12,12,28);
+?>
