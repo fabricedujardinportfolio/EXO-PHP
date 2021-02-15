673183cd334e24f3cce81f12084359b4d6463b59
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-02-15 18:17:17 +1100

    finalisation EXO3 URL4

---
+++ URL4/EXO3/index.php
@@ -0,0 +1,9 @@
+<?php
+function caractere($caractere1,$caractere2)
+{
+    # code...
+    echo "Dans php il y a plusieur façon de voir la vie PAs vrais ? $caractere1 <br> De tout façon soie en aime coder soie en n'aime pas pas vrais ? $caractere2 ";
+}
+caractere("oui","oui");
+
+?>
